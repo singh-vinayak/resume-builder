96c7f91c6d8f986ab07cb136f6b74987597343c1
Vinayak Singh <vinayaksingh5302@gmail.com>
2022-02-20 14:29:01 +0530

    scss fixed

--- src/components/presentation/footer.js
+++ src/components/presentation/footer.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 const footer = () => {
   return (  
   
   <footer className="footer">  
-    <div></div>
+    <div className="text-box">
+      Made with <a href={"https://github.com/vinayak-singh5302"} target="_blank" rel="noreferrer">❤️</a> by Vinayak Singh
+    </div>
   </footer>
 
   );
